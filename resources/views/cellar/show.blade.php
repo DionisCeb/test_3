8df8e4e49d5a349235c4c29e49b454c2535782be
Dionis <e2396498@cmaisonneuve.qc.ca>
2025-01-26 21:07:05 -0500

    backup the main(dionis fork)

--- resources/views/cellar/show.blade.php
+++ resources/views/cellar/show.blade.php
@@ -9,19 +9,38 @@
 
             <form action="" method="GET" class="search-container {{ !empty($query) ? 'expanded' : '' }}" id="search-form">
             <div class="filter-box">
-            <i class="fa-solid fa-filter"></i>
-            <div class="filter-options">
-                <div class="filter-item">
-                <label for="type">Type:</label>
-                <select id="type" name="filter">
-                    <option value="title">Title</option>
-                    <option value="country">Country</option>
-                    <option value="Region">Region</option>
-                    <option value="color">Color</option>
-                </select>
+                    <i class="fa-solid fa-filter"></i>
+                    <div class="filter-options">
+                        <div class="filter-item">
+                            <label for="color">Couleur:</label>
+                            <select id="color" name="color">
+                                <option value="">Tous</option>     
+                                @foreach ($colors as $option)
+                                    <option value="{{ $option }}" {{ $color === $option ? 'selected' : '' }}>{{ $option }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="filter-item">
+                            <label for="country">Pays:</label>
+                            <select id="country" name="country">
+                                <option value="">Tous</option>
+                                @foreach ($countries as $option)
+                                    <option value="{{ $option }}" {{ $country === $option ? 'selected' : '' }}>{{ $option }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="filter-item">
+                            <label for="size">Volume:</label>
+                            <select id="size" name="size">
+                                <option value="">Tous</option>
+                                @foreach ($sizes as $option)
+                                    <option value="{{ $option }}" {{ $size === $option ? 'selected' : '' }}>{{ $option }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
+
                 <input 
                     type="text" 
                     name="search" 
@@ -63,33 +82,12 @@
                     </ul>
                 @endif
                 <p><span>{{ $bottles->total() }}</span>@lang('lang.result_subtitle')</p>
-            @else
-                <!-- Display the default title -->
-                <h2>Vous avez <span>{{ $bottles->total() }} bouteilles</span></h2>
-            @endif
-
-            @if (empty($query))
-                <p><span>Ajouter Les Bouteilles:</span></p>
-                <a href="{{ route('bottle.index') }}" class="btn-border">Ajouter</a>
-            @endif
-        </div>
+                <a href="{{ route('cellar.show', ['cellar' => $cellar->id]) }}" class="btn-border">Retour au cellier</a>
+            </div>
+        @endif
 
         <section class="flex-col gap10">
-            @if ($bottles->isEmpty())
-            <div class="results">
-                    @if (!empty($query))
-                        <h2>Recherche de : "{{ $query }}"</h2>
-                        <p><span>0</span> résultats trouvés</p>
-                        <ul>
-                            <li>Désolé, aucun résultat trouvé dans ce cellier.</li>
-                            <li>Essayez une autre recherche</li>
-                        </ul>
-                        <a href="{{ route('cellar.show', ['cellar' => $cellar->id]) }}" class="btn-border">Retour au cellier</a>
-                    @else
-                        <p>Ce cellier est vide.</p>
-                    @endif
-                </div>
-            @else
+            
                       
             @foreach ($bottles as $bottle)
                 <article class="card_bottle">
@@ -109,25 +107,37 @@
                             <div class="line"></div>
                             <p>{{ $bottle->country }}</p>
                         </div>
+                        <div class="card-list">
+                            @foreach ($cellar_bottles as $cellar_bottle)
+                            @if ($cellar->id == $cellar_bottle->cellar_id && $bottle->id == $cellar_bottle->bottle_id)
+                                <p>@lang('lang.quantity') : {{ $cellar_bottle->quantity }}</p>
+                            @endif
+                            @endforeach
+                        </div>
                        
-                        
-                            <div class="card-list flex flex-col gap5">
+                        <!---the info would be placed in the view of the bottle details of the user-->
+                         <!--<div class="card-list flex flex-col gap5">
                             <p>@lang('lang.region') : {{ $bottle->region }}</p>
                             <p>@lang('lang.degree_alcohol') : {{ $bottle->degree_alcohol }}</p>
                             <p>@lang('lang.sugar_content') : {{ $bottle->sugar_content }}</p>
                             <p>@lang('lang.promoting_agent') {{ $bottle->promoting_agent }}</p>
                         
-                        <p>@lang('lang.producer') : {{ $bottle->producer }}</p>
-                        <p>@lang('lang.grape_variety') : {{ $bottle->grape_variety }}</p>
-                        @foreach ($cellar_bottles as $cellar_bottle)
-                        @if ($cellar->id == $cellar_bottle->cellar_id && $bottle->id == $cellar_bottle->bottle_id)
-                            <div class="quantity">
-                                @lang('lang.quantity') : {{ $cellar_bottle->quantity }}
-                            </div>
-                        @endif
-                        @endforeach
-                            </div>
-                        <a href="{{ route('bottle.details', ['id' => $bottle->id]) }}" class="btn-border">@lang('lang.view')</a>
+                            <p>@lang('lang.producer') : {{ $bottle->producer }}</p>
+                            <p>@lang('lang.grape_variety') : {{ $bottle->grape_variety }}</p>
+
+                            <p>@lang('lang.price') : {{ $bottle->price }}</p>
+                            @foreach ($cellar_bottles as $cellar_bottle)
+                            @if ($cellar->id == $cellar_bottle->cellar_id && $bottle->id == $cellar_bottle->bottle_id)
+                                <p>@lang('lang.quantity') : {{ $cellar_bottle->quantity }}</p>
+                            @endif
+                            @endforeach
+                        </div> -->
+                        
+                        <div class="btn-container">
+                            <a href="{{ route('bottle.details', ['id' => $bottle->id]) }}" class="btn-border">@lang('lang.view')</a>
+                            <a href="{{ route('cellar.remove', ['id' => $bottle->id, 'cellar_id' => $cellar->id]) }}" class="btn-border btn-remove"><i class="fa-solid fa-minus"></i></a>
+                        </div>
+
                     </div>
                 </article>
             @endforeach
